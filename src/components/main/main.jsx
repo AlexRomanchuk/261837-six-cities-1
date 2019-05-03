f3f968ea688f42975d8411a1f521d4b50de20477
Alexander <borom@inbox.ru>
2019-05-03 16:13:30 +0300

    Изменен компонент Main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,41 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Card = (props) => {
-  const {cardTitle} = props;
-  return <article className="cities__place-card place-card">
-    <div className="cities__image-wrapper place-card__image-wrapper">
-      <a href="#">
-        <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image" />
-      </a>
-    </div>
-    <div className="place-card__info">
-      <div className="place-card__price-wrapper">
-        <div className="place-card__price">
-          <b className="place-card__price-value">&euro;132</b>
-          <span className="place-card__price-text">&#47;&nbsp;night</span>
-        </div>
-        <button className="place-card__bookmark-button button" type="button">
-          <svg className="place-card__bookmark-icon" width="18" height="19">
-            <use xlinkHref="#icon-bookmark"></use>
-          </svg>
-          <span className="visually-hidden">To bookmarks</span>
-        </button>
-      </div>
-      <div className="place-card__rating rating">
-        <div className="place-card__stars rating__stars">
-          <span style={{width: `80%`}}></span>
-          <span className="visually-hidden">Rating</span>
-        </div>
-      </div>
-      <h2 className="place-card__name">
-        <a href="#">{cardTitle}</a>
-      </h2>
-      <p className="place-card__type">Apartment</p>
-    </div>
-  </article>;
-};
-
 const Main = (props) => {
   const {names} = props;
   return <div>
@@ -131,7 +96,39 @@ const Main = (props) => {
               </select>
             </form>
             <div className="cities__places-list places__list tabs__content">
-              {names.map((title, i) => <Card key={i} cardTitle={title} />)}
+              {names.map((title, i) =>
+                <article key={i} className="cities__place-card place-card">
+                  <div className="cities__image-wrapper place-card__image-wrapper">
+                    <a href="#">
+                      <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image" />
+                    </a>
+                  </div>
+                  <div className="place-card__info">
+                    <div className="place-card__price-wrapper">
+                      <div className="place-card__price">
+                        <b className="place-card__price-value">&euro;132</b>
+                        <span className="place-card__price-text">&#47;&nbsp;night</span>
+                      </div>
+                      <button className="place-card__bookmark-button button" type="button">
+                        <svg className="place-card__bookmark-icon" width="18" height="19">
+                          <use xlinkHref="#icon-bookmark"></use>
+                        </svg>
+                        <span className="visually-hidden">To bookmarks</span>
+                      </button>
+                    </div>
+                    <div className="place-card__rating rating">
+                      <div className="place-card__stars rating__stars">
+                        <span style={{width: `80%`}}></span>
+                        <span className="visually-hidden">Rating</span>
+                      </div>
+                    </div>
+                    <h2 className="place-card__name">
+                      <a href="#">{title}</a>
+                    </h2>
+                    <p className="place-card__type">Apartment</p>
+                  </div>
+                </article>)
+              }
             </div>
           </section>
           <div className="cities__right-section">
@@ -141,10 +138,6 @@ const Main = (props) => {
       </div>
     </main>
   </div>;
-};
-
-Card.propTypes = {
-  cardTitle: PropTypes.string.isRequired,
 };
 
 Main.propTypes = {
