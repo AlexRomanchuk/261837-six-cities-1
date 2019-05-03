eab5d54f30e68df0dc7605179e3bab0b1f1072ae
Alexander <borom@inbox.ru>
2019-05-03 10:14:53 +0300

    Выполнена проверка PropTypes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./components/main/main.jsx";
 
+const names = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`];
+
 const init = () => {
   ReactDOM.render(
-      <Main />,
+      <Main
+        names={names}
+      />,
       document.querySelector(`#root`)
   );
 };
