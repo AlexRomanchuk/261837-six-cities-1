44d4c25d10b39f9fa7bdd7cfd8a7fb3b0974a142
Alexander <borom@inbox.ru>
2019-05-15 19:49:37 +0300

    Создан тест

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -7,37 +7,42 @@ export default class Map extends PureComponent {
     super(props);
   }
   render() {
-    return <div id="map" style={{height: `100%`}}></div>;
+    return <div id="map" style={{height: `100%`}}>Не удалось показать карту</div>;
   }
   componentDidMount() {
-    const city = [52.38333, 4.9];
+    try {
+      const city = [52.38333, 4.9];
 
-    const icon = leaflet.icon({
-      iconUrl: `img/pin.svg`,
-      iconSize: [27, 39]
-    });
+      const icon = leaflet.icon({
+        iconUrl: `img/pin.svg`,
+        iconSize: [27, 39]
+      });
 
-    const zoom = 12;
+      const zoomCity = 12;
 
-    const map = leaflet.map(`map`, {
-      center: city,
-      zoom: zoom,
-      zoomControl: false,
-      marker: true
-    });
-    map.setView(city, zoom);
+      const map = leaflet.map(`map`, {
+        center: city,
+        zoom: zoomCity,
+        zoomControl: false,
+        marker: true
+      });
+      map.setView(city, zoomCity);
 
-    leaflet
-      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-      })
-      .addTo(map);
-
-    this.props.coords.forEach((coords) => {
       leaflet
-        .marker(coords, {icon})
+        .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+          attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+        })
         .addTo(map);
-    });
+
+      this.props.coords.forEach((coords) => {
+        leaflet
+          .marker(coords, {icon})
+          .addTo(map);
+      });
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
 
